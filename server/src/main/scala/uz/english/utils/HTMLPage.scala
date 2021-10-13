aeb7ef3f18dca639001e0f0f4d907ddebe8d63f0
Maftunbek <maftunbek.r@gmail.com>
2021-10-13 09:04:07 +0500

    read html file fix

--- server/src/main/scala/uz/english/utils/HTMLPage.scala
+++ server/src/main/scala/uz/english/utils/HTMLPage.scala
@@ -2,12 +2,12 @@ package uz.english.utils
 
 import cats.*
 import cats.effect.IO
+import cats.effect.kernel.Async
 import fs2.io.file.{Files, Path}
 import org.http4s.Status.NotFound
 import org.http4s.dsl.Http4sDsl
 import org.http4s.{Request, Response, StaticFile}
 
-import java.io.File
 import scala.concurrent.ExecutionContext
 
 trait HTMLPage[F[_]] {
@@ -19,9 +19,9 @@ object HTMLPage {
 
   def apply[F[_]](implicit ev: HTMLPage[F]): HTMLPage[F] = ev
 
-  private def pathResolver: String => Path = pagePath => Path("uz/english/views/" + pagePath)
+  private def pathResolver: String => String = pagePath => "./views/" + pagePath
 
-  implicit def pageLoader[F[_]: Files: MonadThrow]: HTMLPage[F] =
+  implicit def pageLoader[F[_]: Async]: HTMLPage[F] =
     new HTMLPage[F] {
 
       override def page(
@@ -30,8 +30,7 @@ object HTMLPage {
       )(implicit dsl: Http4sDsl[F]): F[Response[F]] = {
         import dsl.*
         println(pathResolver(pagePath))
-        println(new File("../views/" + pagePath).canRead)
-        StaticFile.fromPath(pathResolver(pagePath), Some(request)).getOrElseF(NotFound())
+        StaticFile.fromResource(pathResolver(pagePath), Some(request)).getOrElseF(NotFound())
       }
     }
 }
